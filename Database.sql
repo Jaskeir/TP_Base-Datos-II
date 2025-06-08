f3e63f4ad6dafa7607cbce06460b5cded3d4be57
Jaskeir <rojasezequielsebastian@hotmail.com>
2025-06-07 23:16:02 -0300

    Actualizacion de Tablas y Vista
    Modifiqué algunas tablas y sus ordenes, de todos modos por lo visto faltan algunas modificaciones. Además agrego la Vista de los datos de los Dueños

--- Database.sql
+++ Database.sql
@@ -8,7 +8,12 @@ Create Table Sedes(
   Direccion varchar(100) not null,
 )
 Go
-
+Create Table Especialidades(
+  IDEspecialidad int not null primary key identity(1, 1),
+  Nombre varchar(20) not null,
+  Descripcion varchar(150) not null
+)
+Go
 Create Table Horarios(
   IDHorario int not null primary key identity (1, 1),
   Fecha datetime not null,
@@ -19,64 +24,54 @@ Create Table Horarios(
 )
 Go
 
-Create Table HorariosVeterinaria(
-  IDHorario int not null primary key references Horarios(IDHorario),
-  IDVeteriantio int not null foreign key references Veterinarios(IDVeteriantio)
-)
-
-Create Table Especialidades(
-  IDEspecialidad int not null primary key identity(1, 1),
-  Nombre varchar(20) not null,
-  Descripcion varchar(150) not null
-)
-Go
-
-Create Table Turnos(
-  IDTurno int not null primary key identity (1, 1),
-  Fecha datetime not null,
-  IDVeteriantio int not null foreign key references Veterinarios(IDVeteriantio),
-  IDMascota int not null foreign key references Mascotas(IDMascota),
-  IDEspecialidad int not null foreign key references Especialidades(IDMascota),
-  IDTelefono int not null,
-  Observaciones Varchar(20) not null
-)
-Go
 
 Create Table Veterinarios(
-  IDVeteriantio int not null primary key identity (1, 1),
+  IDVeterinario int not null primary key identity (1, 1),
   DNI varchar(8) not null,
   Nombre varchar(50) not null,
-  -- Domicilio varchar(80) not null, Nececito realmente el domicilio?
   Email varchar(80) not null
+)
+Go
+Create Table HorariosVeterinaria(
+	ID int not null primary key identity (1, 1),
+  IDHorario int not null foreign key references Horarios(IDHorario),
+  IDVeterinario int not null foreign key references Veterinarios(IDVeterinario)
 )
 Go
 
 Create Table TelefonosVeterinarios(
-  IDVeteriantio int not null primary key references Veterinarios(IDVeteriantio),
+  IDVeterinario int not null primary key references Veterinarios(IDVeterinario),
   Telefono varchar(20) not null,
-)
-Go
-
-Create Table Mascotas(
-  IDMascota int not null primary key identity (1, 1),
-  Nombre varchar(50) not null,
-  Especie varchar(50) not null,
-  --Habitat???
-  IDDueño int not null foreign key references Dueños(IDDueño),
 )
 Go
 
 Create Table Dueños(
-  IDCliente int not null primary key identity (1, 1),
-  DNI varchar(8) not null,
-  Nombre varchar(50) not null,
-  Domicilio varchar(80) not null,
-  Email varchar(80) not null
+    IDDueños int not null primary key identity (1, 1),
+    DNI varchar(8) not null,
+    Nombre varchar(50) not null,
+    Domicilio varchar(80) not null,
 )
 Go
-
+Create Table Mascotas(
+    IDMascota int not null primary key identity (1, 1),
+    Nombre varchar(50) not null,
+    Especie varchar(50) not null,
+    IDDueños int not null foreign key references Dueños(IDDueños),
+)
+Go
 Create Table TelefonosDueños(
-  IDDueño int not null primary key,
-  Telefono varchar(20) not null,
-)
+    IDTelefono int not null primary key identity (1, 1),
+    IDMascota int not null foreign key references Mascotas(IDMascota),
+    Telefono varchar(10) not null,   
+    Observaciones Varchar(20) not null,
+   )
 Go
+Create Table Turnos(
+  IDTurno int not null primary key identity (1, 1),
+  Fecha datetime not null,
+  IDVeterinario int not null foreign key references Veterinarios(IDVeterinario),
+  IDMascota int not null foreign key references Mascotas(IDMascota),
+  IDEspecialidad int not null foreign key references Especialidades(IDEspecialidad),
+  IDTelefono int not null,
+  Observaciones Varchar(20) not null
+)
