1937aef5b7c03ec92973d4ef652b69a6ac602b41
Maruchan <mariannai99@icloud.com>
2025-06-17 00:14:21 -0300

    CAMBIO DE NOMBRE A ID
    cambie los nombres por los ids

--- Vistas/vw_TodosLosTurnos.sql
+++ Vistas/vw_TodosLosTurnos.sql
@@ -5,9 +5,9 @@ Create View vw_TodosLosTurnos AS
 Select 
 		t.IDTurno,				 -- Tomo el ID unico de turno
 		t.FechaHora,			 -- Fecha y hora del turno
-		c.Nombre as Cliente,	 -- nombre cliente que pidio el turno
-		m.Nombre as Mascota,	 -- nombre mascota que sera atendida
-		v.Nombre as Veterinario	 -- nombre veterinario que atndera
+		c.IDCliente,			 -- nombre cliente que pidio el turno
+		m.IDMascota,			 -- nombre mascota que sera atendida
+		v.IDVeterinario			 -- nombre veterinario que atndera
 FROM Turnos t
 Join Mascotas m on t.IDMascota = m.IDMascota
 Join Clientes c on m.IDCliente = c.IDCliente
