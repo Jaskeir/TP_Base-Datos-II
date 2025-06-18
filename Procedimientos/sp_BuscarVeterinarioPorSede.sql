456404f4faa2c50bb3e468361b8181679f0a88af
Tomas1562 <71684235+Tomas1562@users.noreply.github.com>
2025-06-18 16:48:51 -0300

    Cambios precedure sp_BuscarVeterinarioPorSede
    Explicacion de como funciona el procedure

--- Procedimientos/sp_BuscarVeterinarioPorSede.sql
+++ Procedimientos/sp_BuscarVeterinarioPorSede.sql
@@ -1,17 +1,17 @@
-CREATE PROCEDURE sp_BuscarVeterinarioPorSede
-    @IDSede INT
+CREATE PROCEDURE sp_BuscarVeterinarioPorSede  --CREAMOS PROSEDURE
+    @IDSede INT  --PARAMETRO DE ENTRADA ID SEDE
 AS
 BEGIN
     SELECT 
         v.IDVeterinario,
-        v.Nombre AS NombreVeterinario,
+        v.Nombre AS NombreVeterinario,           --SELECCIONAS COLUMNAS DE VARIAS TABLAS
         v.Dni,
         e.DescripcionEspecialidad,
         s.NombreSede
     FROM 
         VETERINARIOS v
     JOIN 
-        TURNOS t ON v.IDVeterinario = t.IDVeterinario
+        TURNOS t ON v.IDVeterinario = t.IDVeterinario     --SE REALIZA CADENA DE JOIN DE VARIAS TABLAS
     JOIN 
         HORARIOS h ON t.IDHorario = h.IDHorario
     JOIN 
@@ -19,13 +19,14 @@ BEGIN
     JOIN 
         ESPECIALIDADES e ON v.IDEspecialidad = e.IDEspecialidad
     WHERE 
-        s.IDSede = @IDSede
+        s.IDSede = @IDSede     --FILTRAMOS POR SEDE
     GROUP BY 
-        v.IDVeterinario, v.Nombre, v.Dni, e.DescripcionEspecialidad, s.NombreSede
+        v.IDVeterinario, v.Nombre, v.Dni, e.DescripcionEspecialidad, s.NombreSede  --AGRUPAMOS
     ORDER BY 
-        v.Nombre;
+        v.Nombre;     --ORDENAMOS POR NOMBRE
 END;
+
+-- PRUEBA PARA QUE DEVUELVA TODOS LOS ID SEDE 2
 
 EXEC sp_BuscarVeterinarioPorSede @IDSede = 2;
 
-SELECT * FROM dbo.TURNOS;
