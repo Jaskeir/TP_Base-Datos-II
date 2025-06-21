e3de439d206f27dc30695391293624bbd6465466
Jaskeir <rojasezequielsebastian@hotmail.com>
2025-06-20 21:07:16 -0300

    Cambios menores para video

---
+++ Consultas ejemplo/Consultas ejemplo.sql
@@ -0,0 +1,5 @@
+select *from vw_TurnosConfirmados 
+select *from vw_VeterinarioPorSede 
+select *from vw_Horarios_Especialidades
+--VALUES (2, 'nuevo_usuario@vet.com', 'clave123nueva');--trigger mail
+
--- Vistas/vw_Disponibilidad.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW vw_Disponibilidad_Especialidades_Veterinarios AS
-SELECT H.DiaSemana, H.HoraDesde, H.HoraHasta, H.IDSede, s.DireccionSede,H.IDEspecialidad,E.Nombre as Especialidad, E.DescripcionEspecialidad , V.IDVeterinario, V.Nombre as 'Nombre Veterinario'
-FROM Horarios H 
-INNER JOIN Sedes S ON H.IDSede = S.IDSede
-INNER JOIN Especialidades E ON H.IDEspecialidad = E.IDEspecialidad
-INNER JOIN Veterinarios V ON H.IDEspecialidad = V.IDEspecialidad;
