05be9c668aa3f61394c876b102e100dad7d7fb3f
Julian Pierri <estudios.w6qo3@silomails.com>
2025-06-22 15:14:04 -0700

    Ordenamientos visuales

--- Consultas-ejemplo/Consultas ejemplo.sql
+++ Consultas-ejemplo/Consultas ejemplo.sql
@@ -1,82 +1,100 @@
-SELECT * FROM Turnos--m
+--Marianna
+SELECT * FROM Turnos
 SELECT * FROM vw_TurnosConfirmados
 WHERE Cliente = 'Marcela Torres';
 SELECT * FROM vw_TurnosConfirmados
 ORDER BY FechaHora;
 SELECT COUNT(*) FROM vw_TurnosConfirmados;
+
 ---------------------------------------------------------
-select *from vw_Horarios_Especialidades--e
+--Ezequiel
+select * from vw_Horarios_Especialidades
 
 select DiaSemana,HoraDesde,HoraHasta,DireccionSede , Especialidad from vw_Horarios_Especialidades where IDEspecialidad=1
 select Especialidad from  vw_Horarios_Especialidades where IDSede=2
 
-
 ---------------------------------------------------------
-select *from vw_VeterinarioPorSede--t 
+--Tomas
+select * from vw_VeterinarioPorSede
 
 select NombreSede from vw_VeterinarioPorSede where IDVeterinario=2
 -----------------------------------------
-select * from vw_Clientes--j
+--Julian
+select * from vw_Clientes
 select Telefono, Observaciones,Nombre Mascota from vw_Clientes where IDMascota=1
+
 ---------------------------------------------------------
-dbo.Permiso(@permisorequerido ,@idUser)--e
---------------
---procedimiento alta horario--m
+--Ezequiel
+dbo.Permiso(@permisorequerido ,@idUser)
+
+---------------------------------------------------------
+--Marianna
+--procedimiento alta horario
 select  * from Horarios
 EXEC sp_AltaHorario
-  @idUsuario = 1,        
-  @DiaSemana = 'Martes',
-  @HoraDesde = '08:00',
-  @HoraHasta = '12:00',
-  @DuracionConsulta = 30,
-  @IDSede = 1,
-  @IDEspecialidad = 1;
+@idUsuario = 1,        
+@DiaSemana = 'Martes',
+@HoraDesde = '08:00',
+@HoraHasta = '12:00',
+@DuracionConsulta = 30,
+@IDSede = 1,
+@IDEspecialidad = 1;
 
-  ----------------------------------------------------
-  --procedimiento SACA TURNO--e
-  select * from Turnos 
-  EXEC sp_SacarTurno
-  @idUsuario=1,
-  @Mascota=2,
-  @Horario=2,
-  @Estado=1,
-  @Veterinario=1,
-  @FechaHora='2025-06-25T11:00:00.000'; 
+----------------------------------------------------
+--Ezequiel
+--procedimiento SACA TURNO
+select * from Turnos 
+EXEC sp_SacarTurno
+@idUsuario=1,
+@Mascota=2,
+@Horario=2,
+@Estado=1,
+@Veterinario=1,
+@FechaHora='2025-06-25T11:00:00.000'; 
 
 --------------------------------------------------------
--- sp_BuscarVeterinarioPorSede--t
- EXEC sp_BuscarVeterinarioPorSede  @IDSede=3
+--Tomas
+-- sp_BuscarVeterinarioPorSede
+EXEC sp_BuscarVeterinarioPorSede  @IDSede=3
 
 -------------------------------------------
---sp_Telefonos--j
- select * froM Telefonos
- EXEC sp_Telefonos @Id=3
+--Julian
+--sp_Telefonos
+select * from Telefonos
+EXEC sp_Telefonos @Id=3
+
 -----------------------------------
---trg_ValidacionDatosTelefono--j
-select * froM Telefonos
+--Julian
+--trg_ValidacionDatosTelefono
+select * from Telefonos
 INSERT INTO Telefonos(IDUsuario, Telefono, Observaciones)
-VALUES
-	(3, '1123963585','')
+       VALUES (3, '1123963585','')
 
 INSERT INTO Telefonos (IDUsuario, Telefono, Observaciones)VALUES (3, '119988766', NULL);
+
 -------------------------------------
---trg_EvitarTurnosDuplicados--m
+--Marianna
+--trg_EvitarTurnosDuplicados
 select * from Turnos 
 INSERT INTO Turnos (IDMascota, IDHorario, IDEstado, IDVeterinario, FechaHora)
 VALUES (1, 1, 2, 2,'2025-06-28T09:00:00.000')
+
 ------------------------------------------
- --trg_ValidarEmailUnico--m
-
-
- select * from  Usuarios
+--Marianna
+--trg_ValidarEmailUnico
+select * from  Usuarios
 INSERT INTO Usuarios (IDPermiso, Email, Clave)
 VALUES (3, 'usuario@example2.com', 'lyz789');
+
 --------------------------------------------
--- trg_ValidarTurnoDentroDeHorario p--t
+--Tomas
+-- trg_ValidarTurnoDentroDeHorario
 INSERT INTO Turnos (IDMascota, IDHorario, IDEstado, IDVeterinario, FechaHora)
 VALUES (1, 1, 2, 2,GETDATE())
+
 -------------------------------------------
---tr_Eliminar_Turno--e
+--Ezequiel
+--tr_Eliminar_Turno
 select * from Turnos 
 delete from Turnos where IDTurno=17
 ---------------------------------------------
